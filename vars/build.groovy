7752195e829b0a755c893c86d0517c470b309d8d
Himateja <azuredevops772@gmail.com>
2026-05-26 17:36:17 +0530

    Update build.groovy

--- vars/build.groovy
+++ vars/build.groovy
@@ -16,5 +16,5 @@ def Deploye(jobname,ip,context)
 
 def testing(jobname)
 {
-   sh "java -jar /var/lib/jenkins/workspace/${jobname}/testing.jar "
+   sh "java -jar /var/lib/jenkins/workspace/${jobname}/FunctionalTesting/testing.jar "
 }
