88a6d7c6898d13ef5b59ba078ca487a3f890ea62
Himateja <azuredevops772@gmail.com>
2026-05-26 16:35:24 +0530

    Update build.groovy

--- vars/build.groovy
+++ vars/build.groovy
@@ -11,5 +11,5 @@ def Build()
 
 def Deploye(jobname,ip,context)
 {
-     sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat10/webapps/${context}.war"
+     sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/myapp.war ubuntu@${ip}:/var/lib/tomcat10/webapps/${context}.war"
 }
